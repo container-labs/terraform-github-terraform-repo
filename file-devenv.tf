b7aa8dfe9cc8fd182167d28a99f758f2859cd043
Will Beebe <will@containerlabs.io>
2021-02-16 05:27:59 +0000

    bump devenv

--- file-devenv.tf
+++ file-devenv.tf
@@ -1,6 +1,6 @@
 module "devenv" {
   source          = "container-labs/devenv-repo/github"
-  version         = "~> 0.1"
+  version         = "~> 0.2"
   repository      = var.repository
   workspace_image = "terraform"
 }
