d2450fd92f1837674d559b4dec1398f38676d647
TF Robot <robot@containerlabs.com>
2021-02-18 10:18:28 +0000

    Committing terraform fmt changes

--- devenv.tf
+++ devenv.tf
@@ -1,8 +1,8 @@
 module "devenv" {
-  source              = "container-labs/devenv-repo/github"
-  version             = "~> 0.6"
-  repository          = var.repository
-  workspace_image     = "terraform"
-  workspace_image_tag = var.workspace_image_tag
+  source                 = "container-labs/devenv-repo/github"
+  version                = "~> 0.6"
+  repository             = var.repository
+  workspace_image        = "terraform"
+  workspace_image_tag    = var.workspace_image_tag
   workspace_image_digest = var.workspace_image_digest
 }
